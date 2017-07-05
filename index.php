6295dbbad76d3af9bd9f6a0fbcb19a7d082d618a
onishield <onishield@gmail.com>
2017-07-05 16:27:05 +0700

    done.

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-HEAD
-Bank is here
-=======
-/*-------- test hello -----------------*/
-jdlfjglkdfjsglkfdslkgfdhsgjhfdjskhgkjdhfskjghkjdfshg
-jdlskfjglkdsfglkjdsflkgjljlkfjsdlkjg
-xnckjhdkhgkjdhsfkjghkjdfshkjghuidshjgkhsdfg
-fdjlgkfsdklghjklflkhnlfkgnhlkjsflkjgdfhsglkjfhdlshgklhdfsghlkjdfsg
-dfnjkgdsfkjhgjkdfhskjghkdjfshgkjdf
-b442ab7d6ce56a71ad4dd43549a929df6b9a
 
-idfhglfdjgoihdfiugoifdhgifdohgofdhlghfdhslg
+/*-------- test hello -----------------*/
+
 ?>
