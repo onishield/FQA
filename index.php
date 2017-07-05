882d044d14add5c2eb1bc6ad6374071473be07e5
onishield <onishield@gmail.com>
2017-07-05 16:19:38 +0700

    dsgf

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-
-<<<<<<< HEAD
-//Bank is here
+HEAD
+Bank is here
 =======
 /*-------- test hello -----------------*/
 jdlfjglkdfjsglkfdslkgfdhsgjhfdjskhgkjdhfskjghkjdfshg
 jdlskfjglkdsfglkjdsflkgjljlkfjsdlkjg
+xnckjhdkhgkjdhsfkjghkjdfshkjghuidshjgkhsdfg
+fdjlgkfsdklghjklflkhnlfkgnhlkjsflkjgdfhsglkjfhdlshgklhdfsghlkjdfsg
 dfnjkgdsfkjhgjkdfhskjghkdjfshgkjdf
->>>>>>> 1043b442ab7d6ce56a71ad4dd43549a929df6b9a
+b442ab7d6ce56a71ad4dd43549a929df6b9a
 
 idfhglfdjgoihdfiugoifdhgifdohgofdhlghfdhslg
 ?>
