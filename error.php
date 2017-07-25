d0107ceba4a04c60a4980f60be43b123825ddc15
GOLFGABB <golfgab@outlook.com>
2017-07-25 14:37:56 +0700

    Push

---
+++ error.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="UTF-8">
+		<title>FQA</title>
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/loginmodel.css">
+	</head>
+	<body>
+		<div id="wrapper">
+			<header class="header-site">
+				<nav class="navbar navbar-inverse navbar-fixed-top">
+					<div class="container">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+
+							</button>
+							<a class="navbar-brand" href="page.php"><img src="Logo.png" width=100px height=30px;></a>
+						</div><!-- navbar-header -->
+
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+            </div>
+          </div>
+
+        </div>
+
+      <div class="panel-body" style="margin-top:120px;">
+      <a href="page.php" style="color:#000000;"><h1 class="error"><b>PAGE NOT FOUND</b></h1></a>
+      <a href="page.php"><h2 class="error" style="left:47%; top:60%;">Click here to return to main page.</h2></a>
+    </div>
+		</body>
+</html>
