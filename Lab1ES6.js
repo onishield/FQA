719a57cfcad6cf0069cf18af433658188b160b19
Wongsathorn Charoenkul <golfgab@outlook.com>
2017-08-30 23:47:47 +0700

    lab2

---
+++ Lab1ES6.js
@@ -0,0 +1,27 @@
+//ex1
+// let a = {'th':'thai','en':'english'};
+// let b = {'th':'siam','jp':'japan','cn':'china'};
+// console.log({a,b});
+// console.log({...a,b});
+// console.log({...a,...b});
+
+//ex2
+// function addNumberToArray(array, number){
+//   for (var i=0; i<array.length; i++) {
+//     newarray =array[i]+number;
+//     }
+//     console.log(newarray);
+// }
+// .0
+// console.log(addNumberToArray([1,2,3],5)); //[6,7,8]
+// console.log(addNumberToArray([1,2,3],1)); //[2,3,4]
+//
+// //ex3
+// function addNumberToArray(array, number){
+//   //Using array.map only!
+//   array.map(x => x + number);
+//   console.log(array);
+// }
+//
+// console.log(addNumberToArray([1,2,3],5)); //[6,7,8]
+// console.log(addNumberToArray([1,2,3],1)); //[2,3,4]
